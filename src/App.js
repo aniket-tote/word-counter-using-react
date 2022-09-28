d2424650b7bd889a7574f084bdd8321d8a3180f2
Aniket <anikettote2001@gmail.com>
2022-09-28 22:35:35 +0530

    removed second page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+// import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+// import Second from './components/Second';
 
 function App() {
   return (
+    // <Router className='flex flex-col'>
+    //   <Navbar/>
+
+    // {/* <Routes>
+    //   <Route path='/' element={ <Textform/>} />
+    //     <Route path='/second' element={<Second/>}>
+          
+    //     </Route>
+    // </Routes> */}
+
+    //   <Footer/>
+    // </Router>
+
     <div className='flex flex-col'>
       <Navbar/>
       <Textform/>
